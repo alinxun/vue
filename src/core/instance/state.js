45df3a0ecaa8d46d33b336787fc2a9b03c8e9849
alinxun <alinxun@qq.com>
2018-06-20 10:18:33 +0800

    状态

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -58,6 +58,7 @@ export function initState (vm: Component) {
   }
   if (opts.computed) initComputed(vm, opts.computed)
   if (opts.watch && opts.watch !== nativeWatch) {
+    // firefox  原生提供Object.prototype.watch 函数
     initWatch(vm, opts.watch)
   }
 }
